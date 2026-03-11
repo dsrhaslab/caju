34fead7f681254dc47d9c0ef856049347265b7ac
André Lucena Ferreira <69969391+Lucenix@users.noreply.github.com>
2026-03-11 17:59:27 +0000

    Update call_wrappers.h

--- caju/metadata/call_wrappers.h
+++ caju/metadata/call_wrappers.h
@@ -4,10 +4,27 @@ class OpenCall {
 
 };
 
-class ReadCall {
+class FOpenCall : public OpenCall {
 
 };
 
+template <class R>
+class Call {
+    virtual R call(FileId& file_id) = 0;
+};
+
+class ReadCall : public Call<ssize_t> {
+    ssize_t call (FileId& file_id);
+};
+
+class FReadCall : public Call<size_t> {
+    size_t call (FileId& file_id);
+};
+
 class WriteCall {
+
+};
+
+class FWriteCall : public WriteCall {
 
 };
